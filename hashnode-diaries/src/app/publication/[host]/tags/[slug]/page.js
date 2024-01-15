e094aa1498376b34bf7de891e8f382df76ea8726
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-01-15 22:13:16 +0530

    separated the hashnode-diaries

--- hashnode-diaries/src/app/publication/[host]/tags/[slug]/page.js
+++
@@ -1,6 +0,0 @@
-import Publication from "@/app/components/blog/Publication";
-const BlogDetailPage = ({ params: { host, slug } }) => {
-  return <Publication host={host} tag={slug} />;
-};
-
-export default BlogDetailPage;
