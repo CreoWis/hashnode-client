a949a73552d8b15bc3c7b8a728173c4be30b71e5
Aakash Patel <aakashpatel25274@gmail.com>
2024-03-10 16:45:11 -0400

    Add checks for access token and host, make GQL call to get publication id, add getBlogInfo query

--- package/src/useHashnodePublishPost.js
+++ package/src/useHashnodePublishPost.js
@@ -1,29 +1,60 @@
 import { publishPost } from "./mutations/post";
+import { getBlogInfo } from "./queries/blog-data";
 import { useState } from "react";
 
 export default function useHashnodePublishPost(settings = {}) {
   const [loading, setLoading] = useState(false);
 
-  const { personalAccessToken } = settings;
-
   const publish = async (data = {}) => {
     const { input, first, endCursor } = data;
+    const { personalAccessToken, host } = settings;
+
+    if (!personalAccessToken) {
+      return {
+        post: null,
+        loading: false,
+        error: "Personal Access Token is required",
+      };
+    }
+
+    if (!host) {
+      return {
+        post: null,
+        loading: false,
+        error: "Host is required",
+      };
+    }
 
     try {
       setLoading(true);
+
+      //Get publication id from blog host
+      const blogInfo = await getBlogInfo(host);
+      const publicationId = blogInfo?.id;
+
+      if (!publicationId) {
+        throw new Error("Publication not found, please check your host.");
+      }
+
+      const finalPayload = {
+        ...input,
+        publicationId,
+      };
+
+      //Call the publishPost mutation with the final payload
       const res = await publishPost(
-        input,
+        finalPayload,
         first,
         endCursor,
         personalAccessToken
       );
+
       return {
         post: res,
         loading,
         error: null,
       };
     } catch (err) {
-      console.error("Error publishing post: ", err);
       return {
         post: null,
         loading,
